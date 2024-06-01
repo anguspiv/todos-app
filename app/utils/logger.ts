3e78a05fec7847ca91836f5a30107718e2bc5972
Angus <angusp@angusp.com>
2024-06-01 16:32:28 -0700

    ci: adds typecheck to gitlab
    Adds the typecheck to the push and pull request workflows for github

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,8 +1,18 @@
 import { createConsola, LogLevels, LogType } from 'consola';
 
+declare global {
+  interface Window {
+    ENV: {
+      [key: string]: string | number | boolean | undefined;
+    };
+  }
+}
+
 const DEFAULT_LEVEL = 'log';
 
-const strLevel: LogType = (window?.ENV?.LOG_LEVEL || DEFAULT_LEVEL).toLowerCase();
+const strLevel: LogType = (
+  (window?.ENV?.LOG_LEVEL as string) || DEFAULT_LEVEL
+).toLowerCase() as LogType;
 
 const level: number = LogLevels[strLevel] || 2;
 
